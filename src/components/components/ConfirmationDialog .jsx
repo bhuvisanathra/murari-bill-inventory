5fef9d937e72f714112e0d66e6fca0078e49dc21
Bhuvnesh Sanathara <68009290+bhuvisanathra@users.noreply.github.com>
2024-03-10 10:47:12 +0530

    Made it responsive in each screen

--- src/components/components/ConfirmationDialog .jsx
+++ src/components/components/ConfirmationDialog .jsx
@@ -3,18 +3,18 @@ import React from "react";
 const ConfirmationDialog = ({ message, onCancel, onConfirm }) => {
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-900 bg-opacity-50">
-      <div className="bg-white p-5 rounded-lg shadow-lg">
+      <div className="bg-white p-5 rounded-lg shadow-lg sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl">
         <p className="mb-5">{message}</p>
-        <div className="flex justify-between">
+        <div className="flex flex-col sm:flex-row justify-between">
           <button
             onClick={onCancel}
-            className="px-4 py-2 bg-gray-500 text-white rounded-md mr-2"
+            className="w-full sm:w-auto mb-2 sm:mb-0 px-4 py-2 bg-gray-500 text-white rounded-md mr-0 sm:mr-2"
           >
             Cancel
           </button>
           <button
             onClick={onConfirm}
-            className="px-4 py-2 bg-red-500 text-white rounded-md"
+            className="w-full sm:w-auto px-4 py-2 bg-red-500 text-white rounded-md"
           >
             Confirm
           </button>
