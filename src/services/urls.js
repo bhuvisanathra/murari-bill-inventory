db59832d4e4003f33c5fee6fc5ce5f09ee3a50e5
Bhuvnesh Sanathara <68009290+bhuvisanathra@users.noreply.github.com>
2024-06-04 09:45:43 +0530

    Updated Link

--- src/services/urls.js
+++ src/services/urls.js
@@ -1,5 +1,4 @@
 // const BASE_URL = "http://localhost:8081";
-const BASE_URL =
-  "https://murari-bill-inventory-backend-production.up.railway.app";
+const BASE_URL = "murari-bill-inventory-backend.railway.internal";
 
 export default BASE_URL;
